d58c115b6e9a65523da7f4ff669c241b6654558a
mmilian <mateusz.milian@gmail.com>
2015-01-29 12:15:42 +0100

    Heroku setup

--- config/env/production.js
+++ config/env/production.js
@@ -33,5 +33,5 @@ var sender = nodemailer.createTransport(({
 module.exports = {
 	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/efektjow_test',
 	mailSender : sender,
-	hostName : "http://powerful-spire-9361.herokuapp.com/"
+	hostName : "http://powerful-spire-9361.herokuapp.com"
 };
