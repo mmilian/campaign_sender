07a4c1371a57ed67dffc7790f209a76c068732ba
mmilian <mateusz.milian@gmail.com>
2015-01-30 16:16:11 +0100

    Heroku setup

--- app/scheduleMessages.js
+++ app/scheduleMessages.js
@@ -19,15 +19,17 @@ var scheduleMessages = {
 			Subscriber.model.find({}, function(err, subscribers) {
 				var messages = [];
 				_.each(subscribers, function(subscriber) {					
-					messages.push(ScheduledMessage({
-						to: subscriber.email,
-						planedSendDate: data.date || Date.now(),
-						campaignId: doc.id,
-						from: doc.from,
-						subject: doc.subject,
-						text: doc.text,
+					if (subscriber.campaigns === undefined || subscriber.campaigns === null  || (_.findIndex(subscriber.campaigns,function(item) {return doc.id == item.campaignId;}) == -1)) {
+						messages.push(ScheduledMessage({
+							to: subscriber.email,
+							planedSendDate: data.date || Date.now(),
+							campaignId: doc.id,
+							from: doc.from,
+							subject: doc.subject,
+							text: doc.text,
 						html: wrapMailInTrackingUrls(doc.html,subscriber.email,subscriber.nick,doc.id)//wrapMailInTrackingUrls && _.isFunction(wrapMailInTrackingUrls) ? wrapMailInTrackingUrls(doc.html) : doc.html
 					}));;
+					}
 				});
 
 				ScheduledMessage.create(messages, function(err) {
